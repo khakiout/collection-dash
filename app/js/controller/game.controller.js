39212e5b19d0e4e61deef1ce7b6dc12233b0bbd5
ruel <rzapanta@stratpoint.com>
2013-11-13 01:02:35 +0800

    added timer for countdown and recreation of tiles

--- app/js/controller/game.controller.js
+++ app/js/controller/game.controller.js
@@ -3,8 +3,11 @@
 game.controller('GameCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 	$scope.tiles = [];
 	$scope.score = 0;
+	$scope.time = 10;
 	
 	function randomizeTiles() {
+		$scope.tiles = [];
+		
 		for (var i = 0; i < 16; i++) {
 			var goods = Math.floor((Math.random()*4));
 			var tile = {
@@ -15,13 +18,42 @@ game.controller('GameCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 			$scope.tiles.push(tile);
 		}
 	}
+	
+	function clearTiles() {
+		for (var i = 0; i < 16; i++) {
+			var tile = $scope.tiles[i];
+			tile.goods = 0;
+		}
+	}
 	randomizeTiles();
 	
+	var generator = setInterval(function(){
+		randomizeTiles();
+    }, 3000);
+
+	var stop;
+	$scope.countdown = function() {
+	    stop = $timeout(function() {
+	      if ($scope.time >= 1) {
+	    	  $scope.time--;
+	          $scope.countdown();
+	      } else {
+	    	  $scope.stop();
+	          $timeout.cancel(stop);
+	      }
+	    }, 1000);
+	  };
+    $scope.countdown();
 	$scope.receive = function(tile) {
 		if (tile.goods != 0) {
 			$scope.score++;
 			tile.goods = 0;
 		}
 		tile.received = true;
+	};
+	
+	$scope.stop = function() {
+		clearTiles();
+		clearInterval(generator);
 	};
 }]);
